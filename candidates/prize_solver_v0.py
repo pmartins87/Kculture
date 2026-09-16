6457d6344223ac0dd9227ac7e50291d38627ce73
SirSpin <pmartinsjr87@gmail.com>
2026-09-16 20:10:55 -0300

    Point Prize Solver candidate wrapper to V3

--- candidates/prize_solver_v0.py
+++ candidates/prize_solver_v0.py
@@ -1,4 +1,4 @@
-"""Hosted-facing candidate wrapper for Prize Solver V2."""
-from solver.prize_solver_v2 import agent
+"""Hosted-facing candidate wrapper for Prize Solver V3."""
+from solver.prize_solver_v3 import agent
 
 __all__ = ["agent"]
